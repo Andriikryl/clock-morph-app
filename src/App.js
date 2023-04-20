0b6e979e5d1bf4d2429d6e40fee48f3aae9ef83d
Andriikryl <andreyk557@gmail.com>
2023-04-20 22:30:43 +0300

    feat[Circle-component]

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import "./App.css";
 function App() {
   const size = 400;
 
-  return <svg viewBox={`0 0 ${size} 400`} width={size} height={size}></svg>;
+  const c = size / 2;
+  const r = (size - 30) / 2;
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/svg/2000"
+      viewBox={`0 0 ${size} 400`}
+      width={size}
+      height={size}
+    ></svg>
+  );
 }
 
 export default App;
---
+++ src/components/Circle.js
@@ -0,0 +1,5 @@
+const Circle = ({ cx, cy, r, className }) => {
+  return <circle cx={cx} cy={cy} r={r} className={className} />;
+};
+
+export default Circle;
